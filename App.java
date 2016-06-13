8550f3bf8610bb74fe4f03259aa680c2530ea1d4
fuentka <fuentka@oocl.com>
2016-06-13 03:53:33 +0000

    just
    Test

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.mnl.app.my_web_app00;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
