2e1c05a8b056c8b2aeb354d8ea33c4bebe8f3b43
Kassandra Fuentes <fuentka@oocl.com>
2016-06-14 20:25:39 +0800

    Module

---
+++ mod01/exercise1/solution/TestNoMain1.java
@@ -0,0 +1,5 @@
+public class TestNoMain1 {
+  public static void main(String[] args) {
+    System.out.println("This program has no 'main' method.");
+  }
+}
---
+++ mod01/exercise1/solution/TestNoMain2.java
@@ -0,0 +1,5 @@
+public class TestNoMain2 {
+  public static void main(String[] args) {
+    System.out.println("This program has no 'main' method.");
+  }
+}
